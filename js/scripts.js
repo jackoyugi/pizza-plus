49018170a6bb99d98ee8e80463d9beedbabb1117
jackoyugi <oyugijack@gmail.com>
2020-05-24 20:14:08 +0300

    added jQuery 'business logic' to the scripts.js and small styles.css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,6 @@
-function Pizza(name, size, crust, toppings, notes) {
-    this.name = name;
-    this.size = size;
-    this.crust = crust;
-    this.toppings = toppings;
-    this.notes = notes;
-}
-$(document).ready(function() {
-    $('form#myPizza').submit(function(event) {
-        event.preventDefault();
-
-        var myName = $('type#name').val();
-        var theSize = [];
-        var theCrust = [];
-        var myToppings = [];
-        var myNotes = $('textarea#notes').val();
-
-        $.each($('input[name="size"]:checked'), function() {
-            theSize.push($(this).val());
-        });
-        $.each($('input[name="crust"]:checked'), function() {
-            thecrust.push($(this).val());
-        });
-        $.each($('input[name="toppings"]:checked'), function() {
-            mytoppings.push($(this).val());
-        });
-        
-        var newpizza = new Pizza(myName, theSize, theCrust, myToppings, myNotes)
-
-        $('div.selected-sizes').append("<button class='btn btn-sm btn-primary'>" + newpizza.size +"</button>")
-
-        
-
-
-    });
-});
+// Business Logic
+function Pizza () {
+    this.toppings = [];
+    this.size;
+    this.total;
+  }

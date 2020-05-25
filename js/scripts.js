06caca1da82cbd4307293b0f09de55f024092d2a
jackoyugi <oyugijack@gmail.com>
2020-05-25 08:49:49 +0300

    added new jQuery effects to scripts.js, so that the user can select pizza of their choices.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,78 @@ function Pizza () {
     this.size;
     this.total;
   }
+  Pizza.prototype.cost = function () {
+    var cost = 0;
+    this.toppings.map(function(topping) {
+      cost += topping
+    });
+    var total = cost + this.size;
+    this.total = "Your total is: " + "$" + total.toFixed(2);
+  };
+  // User Interface Logic
+$(function() {
+    $("#pizzaOrder").submit(function(event) {
+      event.preventDefault();
+      var pizza = new Pizza();
+      $("#pizzaForm").fadeIn();
+      $("#orderStarter button").hide();
+      $(".pizza-image-container").fadeIn();
+      $("button#addToppings").click(function() {
+        var toppingsPrice = parseFloat($("#toppings").val());
+        var toppingsText = $("#toppings option:selected").text();
+        if (toppingsPrice === 0) {
+          $(".error").show();
+          $("#toppings").addClass('error-border');
+        } else if(toppingsPrice === 0.5) {
+          $(".order-receipt").fadeIn();
+          $(".error").hide();
+          $("#toppings").removeClass('error-border');
+          pizza.toppings.push(toppingsPrice);
+          $("#orderConfirmation ul").append('<li>' + toppingsText + '&nbsp;</li>');
+          if(toppingsText === "Pepperoni") {
+            $(".pizza-toppings-container img").remove();
+            $(".pizza-toppings-container").prepend('<img src="imgs/pepperonis.png">');
+          } else if(toppingsText === "Sausage") {
+            $(".pizza-toppings-container img").remove();
+            $(".pizza-toppings-container").prepend('<img src="imgs/sausages.png">');
+          } else if(toppingsText === "Bell Pepper") {
+            $(".pizza-toppings-container img").remove();
+            $(".pizza-toppings-container").prepend('<img src="imgs/bell-pepper.png">');
+          } else if(toppingsText === "Mushroom") {
+            $(".pizza-toppings-container img").remove();
+            $(".pizza-toppings-container").prepend('<img src="imgs/mushroom.png">');
+          } else if(toppingsText === "Onion") {
+            $(".pizza-toppings-container img").remove();
+            $(".pizza-toppings-container").prepend('<img src="imgs/onions.png">');
+          } else if(toppingsText === "Artichoke") {
+            $(".pizza-toppings-container img").remove();
+            $(".pizza-toppings-container").prepend('<img src="imgs/artichokes.png">');
+          } else if(toppingsText === "Tempeh") {
+            $(".pizza-toppings-container img").remove();
+            $(".pizza-toppings-container").prepend('<img src="imgs/tempehs.png">');
+          }
+          $("#toppings").val(0);
+        }
+      });
+      $("#order").click(function() {
+        var size = parseInt($("#sizes").val());
+        if(size === 0) {
+          $(".error").show();
+        } else {
+          $(".error").hide();
+          $("#pizzaForm").fadeOut();
+          $(".pizza-image-container").fadeOut();
+          $(".order-receipt").fadeOut();
+          setInterval(function () {
+            $(".order-receipt").fadeIn();
+            $(".order-receipt ul").addClass('display-block');
+            $(".order-receipt").addClass('text-center');
+            $("#totalCost").text(pizza.total);
+            $("#totalCost").fadeIn();
+          }, 700);
+          pizza.size = size;
+          pizza.cost();
+        }
+      });
+    });
+  });
